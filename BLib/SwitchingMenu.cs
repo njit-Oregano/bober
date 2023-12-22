a6ef09efee2156f90c098959865c4582b0267658
11Firefox11 <t.viktorvagyok@gmail.com>
2023-12-22 18:30:51 +0100

    SwitchingMenu table look added

--- BLib/SwitchingMenu.cs
+++ BLib/SwitchingMenu.cs
@@ -1,15 +1,34 @@
 using Spectre.Console;
+using Spectre.Console.Rendering;
 
 namespace BLib;
 
 public class SwitchingMenu: IRightRenderable {
+    private Layout MainRenderable;
+    private SelectableItem fridge;
+    private SelectableItem games;
     public Layout rendered {
         get {
-            return new Layout("SwitchingMenu");
+            return MainRenderable;
         }
+    }
+    public SwitchingMenu() {
+        MainRenderable = new Layout().Size(3);
+        Table table = new Table();
+        table.AddColumns("Left", "Right");
+        table.Border(TableBorder.None);
+        fridge = new SelectableItem("f", "FRIDGE");
+        fridge.selected = true;
+        games = new SelectableItem("g", "GAMES");
+        table.AddRow(fridge.rendered, games.rendered);
+        table.Columns[0].Centered();
+        table.Columns[1].Centered();
+        table.HideHeaders();
+        MainRenderable.Update(Align.Center(table, VerticalAlignment.Middle));
     }
 
     public bool HandleInput(ConsoleKeyInfo keyInfo) {
-        return false;
+        return true;
     }
+
 }
