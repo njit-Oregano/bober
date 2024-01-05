3d124ce4e735408daf24eb86ba933e84f0730c0e
11Firefox11 <t.viktorvagyok@gmail.com>
2024-01-05 19:50:54 +0100

    Game now renders beavers and zeros
    Co-authored-by: larynx23 <hutyika64@gmail.com>

--- BLib/Game.cs
+++ BLib/Game.cs
@@ -4,6 +4,7 @@ using Spectre.Console;
 public class Game: IRightRenderable {
     public static PossibleGames CurrentGame;
     private Layout MainLayout;
+    private Table MainGrid;
     public Layout rendered {
         get {
             return MainLayout;
@@ -13,6 +14,26 @@ public class Game: IRightRenderable {
     public Game(Character character) {
         Character = character;
         MainLayout = new Layout();
+        MainGrid = new Table();
+        MainGrid.Border(TableBorder.None);
+        List<string> row = new List<string>();
+        for (int i = 0; i < Render.LeftColumnWidth / 2; i++) {
+            row.Add("0");
+            if (i == 3) {
+                row.Add(":beaver:");
+            }
+        }
+        for (int i = 0; i < Render.Height / 2; i++) {
+            if (i == 0) {
+                MainGrid.AddColumns(row.ToArray());
+            } else {
+                MainGrid.AddRow(row.ToArray());
+            }
+        }
+        for (int i = 0; i < Render.LeftColumnWidth / 2; i++) {
+            MainGrid.Columns[i].Padding(0, 0, 0, 0);
+        }
+        MainLayout.Update(Align.Center(MainGrid, VerticalAlignment.Middle));
     }
 
     public bool HandleInput(ConsoleKeyInfo key) {
