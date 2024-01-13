472e48403eaee71b395b5f03bdfc59919f18cb4e
unknown <kevinkeeeh@gmail.com>
2024-01-13 15:11:16 +0100

    New game added

--- BLib/Game.cs
+++ BLib/Game.cs
@@ -1,7 +1,8 @@
 using BLib;
 using Spectre.Console;
 
-public class Game: IRightRenderable {
+public class Game : IRightRenderable
+{
     public static readonly int MaxHeight = Render.Height / 2;
     public static readonly int MaxWidth = Render.LeftColumnWidth / 3;
     private static readonly string BackgroundEmoji = ":blue_square:";
@@ -10,45 +11,59 @@ public class Game: IRightRenderable {
     private Layout MainLayout;
     private Table WrapperTable;
     private Table MainGrid;
-    public Layout rendered {
-        get {
+    public Layout rendered
+    {
+        get
+        {
             return MainLayout;
         }
     }
 
     private readonly Character Character;
     private int[]? PlayerPosition;
-    public int PlayerPositionX {
-        get {
-            if (PlayerPosition == null) {
+    public int PlayerPositionX
+    {
+        get
+        {
+            if (PlayerPosition == null)
+            {
                 return 0;
             }
             return PlayerPosition[0];
         }
     }
-    public int PlayerPositionY {
-        get {
-            if (PlayerPosition == null) {
+    public int PlayerPositionY
+    {
+        get
+        {
+            if (PlayerPosition == null)
+            {
                 return 0;
             }
             return PlayerPosition[1];
         }
     }
     private int _Width;
-    public int Width {
-        get {
+    public int Width
+    {
+        get
+        {
             return _Width;
         }
-        private set {
+        private set
+        {
             _Width = value;
         }
     }
     private int _Height;
-    public int Height {
-        get {
+    public int Height
+    {
+        get
+        {
             return _Height;
         }
-        private set {
+        private set
+        {
             _Height = value;
         }
     }
@@ -60,13 +75,17 @@ public class Game: IRightRenderable {
     public bool GameIsOver = false;
     private int _TickSinceGameOver = 0;
     private int _GravityTick = 0;
-    public int TickSinceGameOver {
-        get {
+    public int TickSinceGameOver
+    {
+        get
+        {
             return _TickSinceGameOver;
         }
-        private set {
+        private set
+        {
             _TickSinceGameOver = value;
-            if (value > 20) {
+            if (value > 20)
+            {
                 GameIsOver = false;
                 _TickSinceGameOver = 0;
                 Render.SetRightToRender(PossibleRightRenderables.Games);
@@ -74,22 +93,49 @@ public class Game: IRightRenderable {
         }
     }
     private int _MoneyEarned = 0;
-    private int MoneyEarned {
-        get {
+    private int MoneyEarned
+    {
+        get
+        {
             return _MoneyEarned;
         }
-        set {
-            if (value < 3) {
-                TickDelayOnBarriers = 3;
-            } else if (value < 6) {
-                TickDelayOnBarriers = 2;
-            } else if (value < 9) {
-                TickDelayOnBarriers = 1;
-            } else {
-                TickDelayOnBarriers = 0;
+        set
+        {
+            if (CurrentGame == PossibleGames.Home)
+            {
+                if (value < 3)
+                {
+                    TickDelayOnBarriers = 2;
+                }
+                else if (value < 6)
+                {
+                    TickDelayOnBarriers = 1;
+                }
+                else
+                {
+                    TickDelayOnBarriers = 3;
+                }
+            }
+            else
+            {
+                if (value < 3)
+                {
+                    TickDelayOnBarriers = 3;
+                }
+                else if (value < 6)
+                {
+                    TickDelayOnBarriers = 2;
+                }
+                else if (value < 9)
+                {
+                    TickDelayOnBarriers = 1;
+                }
+                else
+                {
+                    TickDelayOnBarriers = 0;
+                }
             }
             _MoneyEarned = value;
-            Character.Money += MoneyEarned;
         }
     }
     public int TickDelayOnBarriers;
@@ -97,50 +143,89 @@ public class Game: IRightRenderable {
     private List<ConsoleKeyInfo> InputBuffer = new List<ConsoleKeyInfo>();
 
     private readonly string GameOverText = "Game Over";
-    private int GameOverTextStart {
-        get {
+    private int GameOverTextStart
+    {
+        get
+        {
             return Width / 2 - GameOverText.Length / 2;
         }
     }
-    private int GameOverTextEnd {
-        get {
+    private int GameOverTextEnd
+    {
+        get
+        {
             return GameOverTextStart + GameOverText.Length;
         }
     }
-    public Game(Character character) {
+
+    private bool Jumping = false;
+
+    public Game(Character character)
+    {
         Character = character;
         MainLayout = new Layout();
         MainGrid = new Table();
         WrapperTable = new Table();
     }
 
-    public bool HandleInput(ConsoleKeyInfo key) {
+    public bool HandleInput(ConsoleKeyInfo key)
+    {
         InputBuffer.Add(key);
         return false;
     }
 
-    public bool GameTick() {
-        if (GameIsOver) {
+    public bool GameTick()
+    {
+        if (GameIsOver)
+        {
             GameOver();
             TickSinceGameOver++;
             return true;
         }
         CheckAndMaybePlaceBarriers();
         ProcessInputKeys();
-        for (int i = 0; i < Barriers.Count; i++) {
+        for (int i = 0; i < Barriers.Count; i++)
+        {
             Barriers[i].Tick();
         }
-        if (CurrentGame == PossibleGames.Fly) {
+        if (CurrentGame == PossibleGames.Fly)
+        {
             _GravityTick++;
-            if (_GravityTick >= 4) {
+            if (_GravityTick >= 4)
+            {
                 _GravityTick = 0;
                 SetPlayerPosition(PlayerPositionX, PlayerPositionY + 1);
+            }
+        }
+        if (CurrentGame == PossibleGames.Home)
+        {
+            if (Jumping)
+            {
+                _GravityTick++;
+                if (_GravityTick >= 4)
+                {
+                    SetPlayerPosition(PlayerPositionX, PlayerPositionY - 1);
+                }
+                if (PlayerPositionY == Height - 5)
+                {
+                    Jumping = false;
+                    _GravityTick = 0;
+                }
+            }
+            if (!Jumping)
+            {
+                _GravityTick++;
+                if (_GravityTick >= 6)
+                {
+                    SetPlayerPosition(PlayerPositionX, PlayerPositionY + 1);
+                }
             }
         }
         return true;
     }
 
-    public void SetGameType(PossibleGames game) {
+    public void SetGameType(PossibleGames game)
+    {
         PlayerPosition = null;
         MoneyEarned = 0;
         int[] aspectRatio = GameAspectRatios[game];
@@ -150,12 +235,15 @@ public class Game: IRightRenderable {
         InitPlayerPosition();
     }
 
-    private void InitPlayerPosition() {
-        switch (CurrentGame) {
+    private void InitPlayerPosition()
+    {
+        switch (CurrentGame)
+        {
             case PossibleGames.River:
                 SetPlayerPosition(Width / 2, Height - 1);
                 break;
             case PossibleGames.Home:
+                SetPlayerPosition(4, Height - 1);
                 break;
             case PossibleGames.Fly:
                 SetPlayerPosition(0, Height / 2);
@@ -163,20 +251,24 @@ public class Game: IRightRenderable {
         }
     }
 
-    private void InitGrid() {
+    private void InitGrid()
+    {
         MainLayout.Size(Height);
         MainGrid = new Table();
         MainGrid.Border(TableBorder.None);
         List<string> row = new List<string>();
-        for (int i = 0; i < Width; i++) {
+        for (int i = 0; i < Width; i++)
+        {
             row.Add(BackgroundEmoji);
         }
         MainGrid.AddColumns(row.ToArray());
         MainGrid.HideHeaders();
-        for (int i = 0; i < Height; i++) {
+        for (int i = 0; i < Height; i++)
+        {
             MainGrid.AddRow(row.ToArray());
         }
-        for (int i = 0; i < Width; i++) {
+        for (int i = 0; i < Width; i++)
+        {
             MainGrid.Columns[i].Padding(0, 0, 0, 0);
         }
         WrapperTable = new Table();
@@ -213,7 +305,8 @@ public class Game: IRightRenderable {
 
     private void CheckAndMaybePlaceBarriers()
     {
-        switch (CurrentGame) {
+        switch (CurrentGame)
+        {
             case PossibleGames.River:
                 if (Barriers.Count < 1)
                 {
@@ -226,9 +319,21 @@ public class Game: IRightRenderable {
                 }
                 break;
             case PossibleGames.Home:
+                if (Barriers.Count < 1)
+                {
+                    if (new Random().Next(0, 2) == 0)
+                    {
+                        Barriers.Add(new GameBarrier(this, Width - 1, 0, Height - new Random().Next(1, 4)));
+                    }
+                    else
+                    {
+                        Barriers.Add(new GameBarrier(this, Width - 1, Height - new Random().Next(1, 4), Height));
+                    }
+                }
                 break;
             case PossibleGames.Fly:
-                if (Barriers.Count < 1) {
+                if (Barriers.Count < 1)
+                {
                     int minDistanceBetweenBarriers = TickDelayOnBarriers >= 2 ? 2 : 3;
                     int maxDistanceBetweenBarriers = TickDelayOnBarriers >= 2 ? 6 : 5;
                     int distanceBetweenBarriers = new Random().Next(minDistanceBetweenBarriers, maxDistanceBetweenBarriers);
@@ -240,21 +345,34 @@ public class Game: IRightRenderable {
         }
     }
 
-    private void ProcessInputKeys() {
-        foreach (ConsoleKeyInfo key in InputBuffer) {
-            switch (key.Key) {
+    private void ProcessInputKeys()
+    {
+        foreach (ConsoleKeyInfo key in InputBuffer)
+        {
+            switch (key.Key)
+            {
                 case ConsoleKey.LeftArrow:
-                    if (CurrentGame == PossibleGames.River) {
+                    if (CurrentGame == PossibleGames.River)
+                    {
                         SetPlayerPosition(PlayerPositionX - 1, PlayerPositionY);
                     }
                     break;
                 case ConsoleKey.RightArrow:
-                    if (CurrentGame == PossibleGames.River) {
+                    if (CurrentGame == PossibleGames.River)
+                    {
                         SetPlayerPosition(PlayerPositionX + 1, PlayerPositionY);
                     }
                     break;
                 case ConsoleKey.UpArrow:
-                    if (CurrentGame == PossibleGames.Fly) {
+                    if (CurrentGame == PossibleGames.Home)
+                    {
+                        if (PlayerPositionY == Height - 1)
+                        {
+                            Jumping = true;
+                        }
+                    }
+                    if (CurrentGame == PossibleGames.Fly)
+                    {
                         SetPlayerPosition(PlayerPositionX, PlayerPositionY - 1);
                     }
                     break;
@@ -267,65 +385,86 @@ public class Game: IRightRenderable {
         InputBuffer.Clear();
     }
 
-    public void RemoveBarrier(int position) {
+    public void RemoveBarrier(int position)
+    {
         Barriers.RemoveAll(barrier => barrier.Position == position);
-        switch (CurrentGame) {
+        switch (CurrentGame)
+        {
             case PossibleGames.River:
-                MoneyEarned += 1;
+                MoneyEarned++;
+                Character.Money += 1;
                 break;
             case PossibleGames.Home:
-                MoneyEarned += 2;
+                MoneyEarned++;
+                Character.Money += 2;
                 break;
             case PossibleGames.Fly:
-                MoneyEarned += 3;
+                MoneyEarned++;
+                Character.Money += 3;
                 break;
         }
+        Character.Health += 1;
     }
 
-    public void SetGridCell(int x, int y, string text = "") {
-        if (text == "") {
+    public void SetGridCell(int x, int y, string text = "")
+    {
+        if (text == "")
+        {
             text = BackgroundEmoji;
         }
         MainGrid.UpdateCell(y, x, text);
     }
 
-    private void SetPlayerPosition(int x, int y) {
+    private void SetPlayerPosition(int x, int y)
+    {
         bool barrierIsAtFuturePosition = false;
-        if (CurrentGame == PossibleGames.River) {
-            foreach (GameBarrier barrier in Barriers) {
-                if (barrier.Position == y && (x < barrier.HoleStart || x > barrier.HoleEnd)) {
+        if (CurrentGame == PossibleGames.River)
+        {
+            foreach (GameBarrier barrier in Barriers)
+            {
+                if (barrier.Position == y && (x < barrier.HoleStart || x > barrier.HoleEnd))
+                {
                     barrierIsAtFuturePosition = true;
                     break;
                 }
             }
         }
-        if (x < 0 || x >= Width || y < 0 || y >= Height || barrierIsAtFuturePosition) {
+        if (x < 0 || x >= Width || y < 0 || y >= Height || barrierIsAtFuturePosition)
+        {
             return;
         }
-        if (PlayerPosition != null) {
+        if (PlayerPosition != null)
+        {
             SetGridCell(PlayerPosition[0], PlayerPosition[1]);
         }
         SetGridCell(x, y, Character.Emoji);
         PlayerPosition = new int[] { x, y };
     }
 
-    public void GameOver() {
-        if (!GameIsOver && PlayerPosition != null) {
+    public void GameOver()
+    {
+        if (!GameIsOver && PlayerPosition != null)
+        {
             Barriers.Clear();
-            for (int i = GameOverTextStart; i < GameOverTextEnd; i++) {
+            for (int i = GameOverTextStart; i < GameOverTextEnd; i++)
+            {
                 SetGridCell(i, Height / 2, $"[red bold]{GameOverText[i - GameOverTextStart]}[/]");
             }
         }
         GameIsOver = true;
+        Jumping = false;
         List<string> randomCrackedSymbols = new List<string>() { "!", "@", "#", "$", "%", "^", "&", "*", "(", ")" };
-        for (int i = 0; i < Width; i++) {
-            for (int j = 0; j < Height; j++) {
-                if (j == Height / 2 && i >= GameOverTextStart && i < GameOverTextEnd) {
+        for (int i = 0; i < Width; i++)
+        {
+            for (int j = 0; j < Height; j++)
+            {
+                if (j == Height / 2 && i >= GameOverTextStart && i < GameOverTextEnd)
+                {
                     continue;
                 }
                 SetGridCell(i, j, randomCrackedSymbols[new Random().Next(randomCrackedSymbols.Count)]);
             }
         }
-        
+
     }
 }
--- BLib/GameBarrier.cs
+++ BLib/GameBarrier.cs
@@ -1,55 +1,85 @@
 namespace BLib;
 
-class GameBarrier {
+class GameBarrier
+{
     public static readonly string BarrierEmoji = ":brown_square:";
 
     private Game Game;
     private int TickDelay = 0;
     private int _Position;
-    public int Position {
-        get {
+    public int Position
+    {
+        get
+        {
             return _Position;
         }
-        private set {
-            if (Game.CurrentGame == PossibleGames.River) {
-                if (_Position > 0) {
-                    for (int i = 0; i < Game.Width; i++) {
-                        if (i < HoleStart || i > HoleEnd) {
-                            Game.SetGridCell(i, _Position - 1);
+        private set
+        {
+            switch (Game.CurrentGame)
+            {
+                case PossibleGames.River:
+                    if (_Position > 0)
+                    {
+                        for (int i = 0; i < Game.Width; i++)
+                        {
+                            if (i < HoleStart || i > HoleEnd)
+                            {
+                                Game.SetGridCell(i, _Position - 1);
+                            }
                         }
                     }
-                }
-                if (value > Game.Height) {
-                    Game.RemoveBarrier(Position);
-                } else {
-                    for (int i = 0; i < Game.Width; i++) {
-                        if (i < HoleStart || i > HoleEnd) {
-                            Game.SetGridCell(i, _Position, BarrierEmoji);
-                        } else if (_Position != Game.Height - 1) {
-                            Game.SetGridCell(i, _Position);
+                    if (value > Game.Height)
+                    {
+                        Game.RemoveBarrier(Position);
+                    }
+                    else
+                    {
+                        for (int i = 0; i < Game.Width; i++)
+                        {
+                            if (i < HoleStart || i > HoleEnd)
+                            {
+                                Game.SetGridCell(i, _Position, BarrierEmoji);
+                            }
+                            else if (_Position != Game.Height - 1)
+                            {
+                                Game.SetGridCell(i, _Position);
+                            }
                         }
                     }
-                }
-            } else if (Game.CurrentGame == PossibleGames.Fly) {
-                if (_Position < Game.Width - 1) {
-                    for (int i = 0; i < Game.Height; i++) {
-                        if (i < HoleStart || i > HoleEnd) {
-                            Game.SetGridCell(_Position + 1, i);
+                    break;
+                case PossibleGames.Home:
+                case PossibleGames.Fly:
+                    if (_Position < Game.Width - 1)
+                    {
+                        for (int i = 0; i < Game.Height; i++)
+                        {
+                            if (i < HoleStart || i > HoleEnd)
+                            {
+                                Game.SetGridCell(_Position + 1, i);
+                            }
                         }
                     }
-                }
-                if (value < -1) {
-                    Game.RemoveBarrier(Position);
-                } else {
-                    for (int i = 0; i < Game.Height; i++) {
-                        if (i < HoleStart || i > HoleEnd) {
-                            Game.SetGridCell(_Position, i, BarrierEmoji);
-                        } else if (_Position != 0) {
-                            Game.SetGridCell(_Position, i);
+                    if (value < -1)
+                    {
+                        Game.RemoveBarrier(Position);
+                    }
+                    else
+                    {
+                        for (int i = 0; i < Game.Height; i++)
+                        {
+                            if (i < HoleStart || i > HoleEnd)
+                            {
+                                Game.SetGridCell(_Position, i, BarrierEmoji);
+                            }
+                            else if (_Position != 0)
+                            {
+                                Game.SetGridCell(_Position, i);
+                            }
                         }
                     }
-                }
+                    break;
             }
+            Game.SetGridCell(Game.PlayerPositionX, Game.PlayerPositionY, Character.Emoji);
             CheckCollide();
             _Position = value;
         }
@@ -57,7 +87,8 @@ class GameBarrier {
     public int HoleStart;
     public int HoleEnd;
 
-    public GameBarrier(Game game, int position, int holeStart, int holeEnd) {
+    public GameBarrier(Game game, int position, int holeStart, int holeEnd)
+    {
         Game = game;
         HoleStart = holeStart;
         HoleEnd = holeEnd;
@@ -65,33 +96,43 @@ class GameBarrier {
         Position = position;
     }
 
-    public void Tick() {
-        if (TickDelay < Game.TickDelayOnBarriers) {
+    public void Tick()
+    {
+        if (TickDelay < Game.TickDelayOnBarriers)
+        {
             TickDelay++;
             return;
         }
         TickDelay = 0;
-        if (Game.CurrentGame == PossibleGames.River) {
+        if (Game.CurrentGame == PossibleGames.River)
+        {
             Position++;
-        } else if (Game.CurrentGame == PossibleGames.Fly) {
+        }
+        else if (Game.CurrentGame == PossibleGames.Fly || Game.CurrentGame == PossibleGames.Home)
+        {
             Position--;
         }
     }
 
-    public void CheckCollide() {
-        switch (Game.CurrentGame) {
+    public void CheckCollide()
+    {
+        switch (Game.CurrentGame)
+        {
             case PossibleGames.River:
-                if (Position == Game.PlayerPositionY) {
-                    if (Game.PlayerPositionX < HoleStart || Game.PlayerPositionX > HoleEnd) {
+                if (Position == Game.PlayerPositionY)
+                {
+                    if (Game.PlayerPositionX < HoleStart || Game.PlayerPositionX > HoleEnd)
+                    {
                         Game.GameOver();
                     }
                 }
                 break;
             case PossibleGames.Home:
-                break;
             case PossibleGames.Fly:
-                if (Position == Game.PlayerPositionX) {
-                    if (Game.PlayerPositionY < HoleStart || Game.PlayerPositionY > HoleEnd) {
+                if (Position == Game.PlayerPositionX || Position == Game.PlayerPositionX - 1)
+                {
+                    if (Game.PlayerPositionY < HoleStart || Game.PlayerPositionY > HoleEnd)
+                    {
                         Game.GameOver();
                     }
                 }
